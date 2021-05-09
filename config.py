5cb7415142532081fc60ac5e5f97e12d16ca19c0
Izzatbek <58590849+izzatbekk@users.noreply.github.com>
2021-05-09 21:14:03 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-HEROKU = false   # NOTE Make it false if you're not deploying on heroku.
+HEROKU = False   # NOTE Make it false if you're not deploying on heroku.
 
 # NOTE these values are for heroku.
 if HEROKU:
@@ -15,7 +15,6 @@ if not HEROKU:
     API_HASH = "fe5038b106043cfec0194366da9251c9"
     SUDO_CHAT_ID = -1001278644380
     SUDOERS = [708888699, 13216546]
-    SESSION_STRING = "AgDF7ddL96lyRbBuBtgh1eS-S9-ifI83znMDOjgWKtO6p1FZTCL7Iy_m5QtOa8PueY2h7xtuSLXO2QOVf_g1mrJ6Ee3wsfGO_Vmr2c-bzLbhvVKZaD_R0LSpBv_-zca4VCsuzAV4DA0km15-ab0C3p46HQ-XhlJjuxyt4L5OwfzPyWPoEcn5lWVKFlC9FcoLxSL5-aMKVKudUfzwJX1bPQfI2wM_-1R8amOU0AZ7YkpcLUmf3jYyG7BnEuaZUmdPuTntazwcIM0uzxeb9S-cgS1_ve-pu6OHjIuh8CFTMbWFFg0lyR4fBXn0HoDI-r4Wj2tNQJPNz992r4CjkvZL-u6FbV-IYwA"
 
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
